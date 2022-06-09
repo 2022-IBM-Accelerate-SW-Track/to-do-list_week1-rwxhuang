bc7c8ff56ea6c3dc8e2e330ce5849007fa4f5955
Roderick Huang <rwxhuang@mit.edu>
2022-06-08 17:04:35 -0700

    Completed implementation for About page and To-do list app

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import profile from "../assets/IMG-5087_500x.jpg";
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div class="split left">
+          <div className="centered">
+            <img 
+              className="profile_image"
+              src={profile}
+              alt="Profile Pic"
+              ></img>
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Roderick Huang</div>
+            <div className="brief_description">
+              I'm an MIT undergraduate studying mathematics and computer science! A fun fact about me is that I love to swim.
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
